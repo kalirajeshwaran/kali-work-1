88d3db0d6847d82cb70c12a407253aacb547ea2e
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-06-12 22:24:29 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -74,10 +74,6 @@ async def filter(client, message):
                     btn.append(
                     [InlineKeyboardButton(text=f"{filename}", url=url)]
                 )
-                await asyncio.sleep(10)
-                await nyva.delete()
-                await message.delete()
-
 #                 btn.append(
 #                     [InlineKeyboardButton(text=f"{filename}",callback_data=f"subinps#{file_id}")]
 #                     )
@@ -172,11 +168,7 @@ async def group(client, message):
                     url = resp['shortenedUrl']
                     btn.append(
                     [InlineKeyboardButton(text=f"{filename}", url=url)]
-                )
-                await asyncio.sleep(10)
-                await nyva.delete()
-                await message.delete()
-                    
+                )            
 #                 btn.append(
 #                     [InlineKeyboardButton(text=f"{filename}", url=f"https://telegram.dog/{nyva}?start=pr0fess0r_99_-_-_-_{file_id}")]
 #                 )
