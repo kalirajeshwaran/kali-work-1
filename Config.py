7d70cb14f5d710fa796be3c6af18cbddb9553f12
AK4111 <98637357+AK4111@users.noreply.github.com>
2022-07-28 19:30:35 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -27,7 +27,7 @@ AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('FORCES_SUB')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
-TUTORIAL = "https://mdisk.me/convertor/9x20/AiRLIm"
+TUTORIAL = "https://t.me/+H_E3OYggvXU4MTk1"
 # MongoDB information
 DATABASE_URI = environ['DATABASE_2']
 DATABASE_NAME = environ['BOT_NAME']
